c1cf7bf866787dadb363b400a35da0a66f265655
prakashydv01 <135949364+prakashydv01@users.noreply.github.com>
2025-06-21 13:10:54 +0545

    vite config added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        // List problematic dependencies here
+        'react',
+        'react-dom',
+        'react-router-dom',
+        // Add other modules causing issues
+      ],
+    },
+  },
+});
