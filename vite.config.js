9ccedfeee38e39284a313057f7eb1445b6312d32
prakashydv01 <135949364+prakashydv01@users.noreply.github.com>
2025-06-22 16:19:16 +0545

    sitemap added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import sitemap from 'vite-plugin-sitemap';
+
 
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [
+    react(),
+    visualizer(),
+    sitemap({
+      hostname: 'https://www.hamroexam.com',
+      routes: [
+        '/',
+        '/about',
+        '/contact',
+        '/privacy-policy',
+        '/terms',
+        '/blog',
+        '/practice-guides',
+        '/medical',
+        '/csit',
+        '/bit',
+        '/bca',
+        '/ioe',
+        '/pioe',
+        'Pcsit',
+        '/PBit',
+        '/Pmedical',
+        '/login',
+        '/signup'
+      ]
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
