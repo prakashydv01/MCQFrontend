bbbc97dc2ef02236a89b139264e433e014c86ddc
prakashydv01 <135949364+prakashydv01@users.noreply.github.com>
2025-08-30 09:46:08 +0545

    sitemap added in public

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
-import sitemap from 'vite-plugin-sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -9,31 +8,6 @@ export default defineConfig({
     react(),
     visualizer(),
     tailwindcss(),
-    sitemap({
-      hostname: 'https://www.hamroexam.com',
-      gzip: true,
-      changefreq: 'daily',
-      priority: 0.8,
-      dynamicRoutes: [
-        '/',
-        '/about',
-        '/contact',
-        '/privacy-policy',
-        '/terms',
-        '/blog',
-        '/guidance',
-        '/medical',
-        '/mock-test/csit',
-        '/mock-test/bit',
-        '/mock-test/ioe',
-        '/practice/ioe',
-        '/practice/csit',
-        '/practice/bit',
-        '/loksewa/computer-operator',
-        '/login',
-        '/signup'
-      ]
-    })
   ],
   build: {
     rollupOptions: {
